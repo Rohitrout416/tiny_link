fc46b9d34818eaf85e46a4d6a92e24482967c5f1
Rohit Kumar Rout <79717560+Rohitrout416@users.noreply.github.com>
2025-11-22 19:20:18 +0530

    Modified the position of the logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export function Header() {
     return (
         <header className="sticky top-0 z-50 w-full border-b border-slate-200 bg-linear-to-l from-white to-[rgba(255,134,1,1)] shadow-sm">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="w-full px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-24">
                     <Link href="/" className="flex items-center group">
                         <div className="flex items-center gap-2">
